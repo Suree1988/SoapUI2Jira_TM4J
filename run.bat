1cbb26ba0362ce77e65bad4fe301954998db1cee
Suree1988 <44668926+Suree1988@users.noreply.github.com>
2019-12-27 11:27:10 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-javac -cp ./lib/*  *.java
+javac -cp ./lib/*  src/com/tm4j/myFirstSoapUITest.class.java src/com/tm4j/TestRunner.class
 java -cp ./bin;./lib/* org.junit.runner.JUnitCore com.tm4j.TestRunner
